123095fa9a1d8620956e1b7dbaaa0da8e0e1c2f4
Jeff <jufu.peng@360jie.com.cn>
2018-03-26 19:11:10 +0800

    Initial project

---
+++ src/main/java/com/test/spc/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.test.spc.controller;
+
+import com.test.spc.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by JeffPeng on 2018/3/26.
+ */
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+}
