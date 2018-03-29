4d2fbb9b18f41865e56fbe8282621a48b748956b
Jeff <jufu.peng@360jie.com.cn>
2018-03-29 22:29:42 +0800

    配置中心动态更新

--- src/main/java/com/test/spc/controller/HelloController.java
+++ src/main/java/com/test/spc/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.test.spc.controller;
 import com.test.spc.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by JeffPeng on 2018/3/26.
  */
 @RestController
+@RefreshScope
 public class HelloController {
     @Autowired
     HelloService helloService;
