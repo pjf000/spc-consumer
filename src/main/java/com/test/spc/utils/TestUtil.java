578b794c991c1226567a5d13d9a6894faba98190
Jeff <jufu.peng@360jie.com.cn>
2018-03-30 18:56:44 +0800

    test类

--- src/main/java/com/test/spc/utils/TestUtil.java
+++ src/main/java/com/test/spc/utils/TestUtil.java
@@ -1,7 +1,22 @@
 package com.test.spc.utils;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 /**
  * Created by JeffPeng on 2018/3/30.
  */
 public class TestUtil {
+
+    public static void main(String args[]){
+        System.out.println("——————————————————————————————");
+        List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        filter(features,(s)->s.endsWith("I"));
+    }
+    public static void filter(List<String> names, Predicate<String> condition) {
+        names.stream().filter((name) -> (condition.test(name))).forEach((name) -> {
+            System.out.println(name + " ");
+        });
+    }
 }
